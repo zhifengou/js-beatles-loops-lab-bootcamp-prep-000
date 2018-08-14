caf71369a75a4b163b0e5ef934facbf81c5aa595
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-14 02:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr=[]
+  for(var i=0;i<musicians.length;i++){
+    arr.push(musicians[i]+" plays "+instruments[i])
+  }
+  return arr
+}
+function johnLennonFacts(arr){
+  var str=[]
+  var i=0
+  while(i<arr.length){
+    str.push(arr[i]+"!!!")
+  }
+  return str
+}
